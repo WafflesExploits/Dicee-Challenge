fb36c59d42eb62b9b52182872767ca87cc25ed58
André oliveira <andreoliveira208@gmail.com>
2022-11-15 13:48:10 +0000

    GetWinner

--- index.js
+++ index.js
@@ -25,3 +25,21 @@ function randomizedDice(){
     document.querySelectorAll("img")[1].setAttribute("src","imgs/dice"+dice2+".png");
   }
 }
+function getWinner(){
+
+  getDicee();
+  if(dice1>dice2){
+    document.querySelectorAll("h1")[0].textContent = "🚩 Play 1 Wins!";
+    document.querySelectorAll("img")[0].setAttribute("src","imgs/dice"+dice1+".png");
+    document.querySelectorAll("img")[1].setAttribute("src","imgs/dice"+dice2+".png");
+  }else if(dice1<dice2){
+    document.querySelectorAll("h1")[0].textContent = "Play 2 Wins! 🚩";
+    document.querySelectorAll("img")[0].setAttribute("src","imgs/dice"+dice1+".png");
+    document.querySelectorAll("img")[1].setAttribute("src","imgs/dice"+dice2+".png");
+  }else{
+    document.querySelectorAll("h1")[0].textContent = "🏳️ Draw! 🏳️";
+    document.querySelectorAll("img")[0].setAttribute("src","imgs/dice"+dice1+".png");
+    document.querySelectorAll("img")[1].setAttribute("src","imgs/dice"+dice2+".png");
+  }
+}
+getWinner();
